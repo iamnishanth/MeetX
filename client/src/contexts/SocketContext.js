f3cfb5faead329e131bc2f7b5e0100d753eef21f
Nishanth <63568590+iamnishanth@users.noreply.github.com>
2023-11-14 22:28:08 +0530

    Migrate from heroku to render

--- client/src/contexts/SocketContext.js
+++ client/src/contexts/SocketContext.js
@@ -4,7 +4,7 @@ import Peer from "simple-peer";
 
 const SocketContext = createContext();
 
-const socket = io("https://meetx-backend.herokuapp.com/");
+const socket = io("https://meetx.onrender.com");
 // const socket = io("http://localhost:5000");
 
 const ContextProvider = ({ children }) => {
